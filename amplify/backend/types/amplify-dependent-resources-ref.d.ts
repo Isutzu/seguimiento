d1a4820b095bf76e10dbcde25fad6cd0a29a3b28
oscar rodriguez <oscar@oscars-MacBook-Air.local>
2022-12-22 23:43:36 -0800

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tracker70a77178": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "geo": {
+        "trackerMap": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {MapView} from '@aws-amplify/ui-react';
+import {Amplify} from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='App'>
+  <h2> Este es el mapita</h2>
+    <MapView
+					initialViewState={{
+						longitude: -122.3381659,
+						latitude: 47.615686,
+						zoom: 12,
+					}}
+					style={{ width: '600px', height: '600px' }}
+				/>
+</div>
   );
 }
 
