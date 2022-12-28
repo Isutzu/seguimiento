59c3b5d90ee7f99dba9a86daab03e1b4f65c857e
oscar rodriguez <oscar@oscars-MacBook-Air.local>
2022-12-28 01:05:31 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import './App.css';
 import {MapView} from '@aws-amplify/ui-react';
+import {Map} from './Map'
 import {Amplify} from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
-
-Amplify.configure(awsExports);
+//Amplify.configure(awsExports);
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-west-2:9513e82a-eb54-4b54-8be7-c5a4bd1f2ca3",
+    region: "us-west-2",
+  },
+  geo: {
+    AmazonLocationService: {
+      maps: {
+        items: {
+          "trackerMap-dev": {
+            style: "Default style"
+          },
+        },
+        default: "trackerMap-dev",
+      },
+      region: "us-west-2",
+    },
+  }
+});
 
 function App() {
-  return (
-<div className='App'>
-  <h1> Este es el mapita</h1>
-    <MapView
-					initialViewState={{
-						latitude: -12.059246,
-            longitude: -77.034727,
-            zoom: 15,
-					}}
-					style={{ width: '900px', height: '600px' }}
-				/>
-</div>
-  );
-}
+  return(
+    <div>
+    <h1> este es el mapa</h1>
+    <Map />
+    </div>
+  )
+  }
+// function App() {
+//   return (
+//        <div className='App'>
+//   <h1> Este es el nuevo mapa</h1>
+//     <MapView
+// 					initialViewState={{
+// 						latitude: -12.059246,
+//             longitude: -77.034727,
+//             zoom: 15,
+// 					}}
+// 					style={{ width: '900px', height: '600px' }}
+// 				/>
+// </div> 
+
+// )}
+  
+        
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Amplify} from 'aws-amplify'
-import { AmplifyProvider } from '@aws-amplify/ui-react'
-import config from './aws-exports'
 import './index.css'
 import '@aws-amplify/ui-react/styles.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-Amplify.configure(config);
 
-// Amplify.configure({
-//   Auth: {
-//     identityPoolId: "us-west-2:9513e82a-eb54-4b54-8be7-c5a4bd1f2ca3",
-//     region: "us-west-2",
-//   },
-//   geo: {
-//     AmazonLocationService: {
-//       maps: {
-//         items: {
-//           "trackerMap-dev": {
-//             style: "Default style"
-//           },
-//         },
-//         default: "trackerMap-dev",
-//       },
-//       region: "us-west-2",
-//     },
-//   }
-// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
